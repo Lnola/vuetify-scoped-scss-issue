fbf4c7cac8a7c842b9110c7440b1a2d1a424eec8
Lnola <nola.luka@gmail.com>
2024-03-14 16:26:12 +0100

    :lipstick: Add custom scss variables

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,13 @@ import WSheet from './WSheet.vue';
 import VvSheet from './VvSheet.vue';
 import { color, width, height } from './config';
 </script>
+
+<style lang="scss">
+.v-application {
+  background-color: $blue-custom !important;
+
+  @media #{map-get($display-breakpoints, 'md-and-up')} {
+    margin: 1rem 0 0;
+  }
+}
+</style>
